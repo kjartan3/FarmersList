741f227283cea1d3d502acf19e5ac9c0f699ef34
Kjartan Heimisson <kheimisson@gmail.com>
2024-10-21 17:24:32 +0100

    initial commit 4

---
+++ classes/Auth.js
@@ -0,0 +1,22 @@
+const Customer = require('./Customer')
+
+class Auth {
+    constructor() {
+        this.customers = []
+    }
+    register(name, email, shippingAddress) {
+        const newCustomer = new Customer(name, email, shippingAddress);
+        this.customers.push(newCustomer);
+    }
+    login(email) {
+        for (let i = 0; i < this.customers.length; i++) {
+            if (this.customers[i].email === email) {
+                return this.customers[i];
+            }
+        }
+        return null
+    }
+}
+
+
+module.exports = Auth;
